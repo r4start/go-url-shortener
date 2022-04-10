05f1ceb7ddc0df6d65f34c99892e79a4c1624eef
r4start <r4start@protonmail.ch>
2022-04-10 20:31:29 +0300

    Added gzip encoding parsing.

--- internal/app/shortener.go
+++ internal/app/shortener.go
@@ -36,6 +36,9 @@ func NewURLShortener(domain, fileStoragePath string) (*URLShortener, error) {
 		st = storage.NewInMemoryStorage()
 	}
 	handler := &URLShortener{Mux: chi.NewMux(), urlStorage: st, domain: domain}
+
+	handler.Use(UnpackGzip)
+
 	handler.Get("/{id}", handler.getURL)
 	handler.Post("/", handler.shorten)
 	handler.Post("/api/shorten", handler.apiShortener)
@@ -154,3 +157,20 @@ func (h *URLShortener) makeResultURL(r *http.Request, data []byte) string {
 	}
 	return fmt.Sprintf("http://%s/%s", r.Host, string(data))
 }
+
+//func readRequestBody(r *http.Request) ([]byte, error) {
+//	var reader io.Reader
+//
+//	if r.Header.Get("Content-Encoding") == "gzip" {
+//		gz, err := gzip.NewReader(r.Body)
+//		if err != nil {
+//			return nil, err
+//		}
+//		defer gz.Close()
+//		reader = gz
+//	} else {
+//		reader = r.Body
+//	}
+//
+//	return io.ReadAll(reader)
+//}
